6abfa043e5feba7e4825340d4de6e4f9716cedc5
Mulia Arifandi Nasution <mul14@users.noreply.github.com>
2015-12-03 10:52:42 +0700

    Fix wrong variable

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@ $message = $_POST['message'];
 
 $headers  = 'MIME-Version: 1.0' . CRLF;
 $headers .= 'Content-type: text/html; charset=utf-8' . CRLF;
-$headers .= "From: {$from_name} <{$to_email}>" . CRLF;
+$headers .= "From: {$from_name} <{$from_email}>" . CRLF;
 $headers .= 'To: Inteligra <hello@inteligra.co>' . CRLF;
 
 mail($to, $subject, $message, $headers);
